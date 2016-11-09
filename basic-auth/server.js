a5199aff425cbcff753246ccc7a46bd6be6d247d
davidmgudeman <davidmgudeman@gmail.com>
2016-11-08 18:14:28 -0800

    Week03 cookie third heading exercise 1 - cookies completed

--- basic-auth/server.js
+++ basic-auth/server.js
@@ -5,6 +5,8 @@ var express = require('express');
 /* morgan is a middleware that supports logging */
 var morgan = require('morgan');
 
+var cookieParser = require('cookie-parser');
+
 
 /***********************************************************************************/
 
@@ -15,34 +17,49 @@ var port = 3000;
 var app = express();
 
 /*************************************************************************************/
-
+/* This add logging functionality */
 app.use(morgan('dev'));
 
+app.use(cookieParser('12345-67890-09876-54321')); // secret key
 /**************************************************************************************/
 /* Creating a specific middleware called auth. We will insert the code that does basic authentication.
 */
-function auth (req, res, next) {
+function auth(req, res, next) {
     console.log(req.headers);
-    
-    // collect the req Header that has the username and password.
-    var authHeader = req.headers.authorization;
-    if(!authHeader) {
-        var err = new Error('You are not authenticated!');
-        err.status = 401;
-        next(err);
-        return;
-    }
-    var auth = new Buffer(authHeader.split(' ')[1],
-    'base64').toString().split(':');
-    var user = auth[0];
-    var pass = auth[1];
-    if (user == 'admin' && pass == 'password') {
-        next(); // authorized
+
+    if (!req.signedCookies.user) {
+        // collect the req Header that has the username and password.
+        var authHeader = req.headers.authorization;
+        if (!authHeader) {
+            var err = new Error('You are not authenticated!');
+            err.status = 401;
+            next(err);
+            return;
+        }
+        var auth = new Buffer(authHeader.split(' ')[1],
+            'base64').toString().split(':');
+        var user = auth[0];
+        var pass = auth[1];
+        if (user == 'admin' && pass == 'password') {
+            /* res.cookie([cookie name],[value], [signed]); */
+            res.cookie('user', 'admin', { signed: true });
+            next(); // authorized
+        } else {
+            var err = new Error('You are not authenticated!');
+            err.status = 401;
+            next(err);
+        }
     } else {
-        var err = new Error('You are not authenticated!');
-        err.status = 401;
-        next(err);
-    } 
+        if(req.signedCookies.user === 'admin'){
+            //console.log(req.signedCookies);
+            next();
+        } else {
+            
+            var err = new Error('You are not authenticated!');
+            err.status = 401;
+            next(err);
+        }
+    }
 }
 
 app.use(auth);
@@ -62,18 +79,18 @@ app.use(express.static(__dirname + '/public'));
 /**********************************************************************************************/
 //insert a middleware function that handles the Error
 
-app.use(function (err,req,res,next) {
-    res.writeHead(err.status || 500, {
-        'WWW-Authenticate': 'Basic',
-        'Content-Type': 'text/plain'
-    });
+app.use(function (err, req, res, next) {
+     res.writeHead(err.status || 500, {
+            'WWW-Authenticate': 'Basic',
+            'Content-Type': 'text/plain'
+        });
     res.end(err.message);
-    
+
 });
 /**********************************************************************************************/
 
 /* With express you no longer have to call create http server. The app.listen replaces that. The
  app object takes care of registering with the http server.*/
-app.listen(port, hostname, function() {
+app.listen(port, hostname, function () {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
